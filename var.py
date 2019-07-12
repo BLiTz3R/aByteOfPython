068fecb10544f392cdd3dfefc00b6e86ee73c38d
BLiTz3R <bl1723r@gmail.com>
2019-07-12 10:45:42 +0300

    update

---
+++ var.py
@@ -0,0 +1,14 @@
+# Filename : var.py
+i = 5
+print i
+i = i + 1
+print i
+s = '''This is a multi-line string.
+This is the second line.'''
+print s
+# if you have a long line
+# of code, you can break it into multiple physical lines by using the backslash. This is
+# referred to as explicit line joining:
+s = 'This is a string. \
+This continues the string.'
+print s
